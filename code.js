3599742f5450dc31a0c63ea8fe94d0a8d5268357
bnb <bnbtrikala@gmail.com>
2024-02-01 21:45:26 +0200

    Finish button

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 console.log('hi');
 const container = document.querySelector('.container');
+let rows = 16;
 
-for (let i = 1; i <= 15; i++) {
+for (let i = 1; i <= rows; i++) {
     let row = document.createElement('div');
     row.className = "row";
     container.appendChild(row);
@@ -16,3 +17,28 @@ for (let i = 1; i <= 15; i++) {
 
     console.log('create Div');
 }
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    const canvaSize = prompt('Give the canva size (1-100)!!!!');
+    const row = document.querySelector('.row');
+    console.log(row);
+    if (canvaSize > 0 && canvaSize <= 100 ) {
+        for ( let i = 1; i <= rows; i++  ) {
+        const row = document.querySelector('.row');
+          container.removeChild(row);    
+        }
+        
+    }
+    console.log(canvaSize);
+})
+
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = 'red';
+        console.log('hover');
+    })
+})
