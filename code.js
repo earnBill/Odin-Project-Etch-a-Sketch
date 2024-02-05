5935e947b1442c0e949d1e2a1920b1dd2ebb39b7
billTas <billtas@gmail.com>
2024-02-05 19:38:47 +0200

    Finish Main Project

--- code.js
+++ code.js
@@ -1,44 +1,51 @@
 console.log('hi');
 const container = document.querySelector('.container');
 let rows = 16;
+createCamva(rows);
+paint();
 
-for (let i = 1; i <= rows; i++) {
-    let row = document.createElement('div');
-    row.className = "row";
-    container.appendChild(row);
-    
+function createCamva(pixels) {
+    for (let i = 1; i <= pixels; i++) {
+        let row = document.createElement('div');
+        row.className = "row";
+        container.appendChild(row);
+        
+        for (let j = 1; j <= pixels; j++) {
+            let pixel = document.createElement('div');
+            pixel.className = "pixel";
 
-    for (let j = 1; j <= 15; j++) {
-        let pixel = document.createElement('div');
-        pixel.className = "pixel";
-
-        row.appendChild(pixel);
+            row.appendChild(pixel);
+        }
+        console.log('create Div');
     }
-
-    console.log('create Div');
 }
 
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
     const canvaSize = prompt('Give the canva size (1-100)!!!!');
-    const row = document.querySelector('.row');
-    console.log(row);
+    const rows = document.querySelectorAll('.row');
+    
     if (canvaSize > 0 && canvaSize <= 100 ) {
-        for ( let i = 1; i <= rows; i++  ) {
-        const row = document.querySelector('.row');
-          container.removeChild(row);    
-        }
-        
+        for (let row of rows ) {
+            container.removeChild(row);            
+        }   
     }
-    console.log(canvaSize);
+        else {
+            alert("Please choose a range between 1-100!!!!");
+        } 
+    createCamva(canvaSize);
+    paint();
 })
 
-const pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = 'red';
-        console.log('hover');
+function paint() {
+    const pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'red';
+            console.log('hover');
+        })
     })
-})
+}
